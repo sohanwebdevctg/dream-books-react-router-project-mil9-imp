605a711f739b9977e3a10c6ef324ee4e1e6cf3f3
sohanwebdevctg <amirhossainsohan0@gmail.com>
2023-09-17 21:54:30 +0600

    create Home,Books,AboutUs and Footer page

--- src/components/BookDetails/BookDetails.jsx
+++ src/components/BookDetails/BookDetails.jsx
@@ -8,7 +8,12 @@ const BookDetails = () => {
   const {image,desc,publisher,authors,year,title,price,rating} = bookDetails;
   return (
     <div>
-      
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-10'>
+      <div className='flex justify-center'>
+        <img className='w-96 h-96' src={image}></img>
+      </div>
+      <div></div>
+    </div>
     </div>
   );
 };
