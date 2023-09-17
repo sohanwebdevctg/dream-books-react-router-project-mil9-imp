cba489139e8c16f46036c5f89e2cf0435cef5621
sohanwebdevctg <amirhossainsohan0@gmail.com>
2023-09-17 22:23:03 +0600

    adding lottie image with animation

--- src/components/BookDetails/BookDetails.jsx
+++ src/components/BookDetails/BookDetails.jsx
@@ -5,6 +5,7 @@ import { useLoaderData } from 'react-router-dom';
 
 const BookDetails = () => {
   const bookDetails = useLoaderData();
+  console.log(bookDetails)
   const {image,desc,publisher,authors,year,title,price,rating} = bookDetails;
   return (
     <div className='p-20'>
@@ -12,8 +13,11 @@ const BookDetails = () => {
       <div className='border-2 border-solid border-blue-800 bg-gray-500'>
         <img className='image w-auto h-auto' src={image}></img>
       </div>
-      <div className=' bg-white'>
-        <img className='image w-auto h-auto' src={image}></img>
+      <div className=' bg-blue-800 flex items-center justify-center'>
+        <div>
+          <span className='text-base font-semibold bg-gray-500 p-2 rounded-2xl text-slate-200'>BRAND NEW</span>
+          <h5 className='text-4xl font-bold my-3 text-yellow-400'>{title ? title : 'No Data'}</h5>
+        </div>
       </div>
     </div>
     </div>
