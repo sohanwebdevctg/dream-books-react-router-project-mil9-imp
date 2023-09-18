db946e205411c84ae12fed277053e78274c6c54c
sohanwebdevctg <amirhossainsohan0@gmail.com>
2023-09-18 12:41:40 +0600

    cahnging book font size

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -15,11 +15,11 @@ const Book = ({book}) => {
       <div className=''>
         <img className='object-cover md:w-96 md:h-64 w-auto h-auto' src={image}></img>
       </div>
-      <div className='bg-gray-600 hover:opacity-80 opacity-0 absolute top-0 left-0 bottom-0 right-0 duration-500 p-6 flex flex-col'>
-        <h1 className='text-slate-100 text-xl'>Title : {title ? title : 'No-Data'}</h1>
+      <div className='bg-gray-600 hover:opacity-80 opacity-0 absolute top-0 left-0 bottom-0 right-0 duration-500 p-2 md:p-6 flex flex-col'>
+        <h1 className='text-slate-100 md:text-xl'>Title : {title ? title : 'No-Data'}</h1>
         {/* // eslint-disable-next-line react/prop-types */}
-        <p className='text-slate-100 text-lg py-2'>SubTitle : {subtitle ? subtitle.slice(0,60) : 'No-Data'}...</p>
-        <p className='text-slate-100 text-sm py-2 mt-auto'>Price : {price ? price : 'No-Data'}</p>
+        <p className='text-slate-100 md:text-lg md:py-2'>SubTitle : {subtitle ? subtitle.slice(0,60) : 'No-Data'}...</p>
+        <p className='text-slate-100 md:text-sm py-2 mt-auto'>Price : {price ? price : 'No-Data'}</p>
       </div>
     </div>
     </Link>
